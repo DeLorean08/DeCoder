b0ffe6422a9fb13342c2bb6f2da929eeb1e65ae7
DeLorean08 <kepnono@gmail.com>
2026-04-19 10:49:53 +0300

    chore: remove redundant comments in models init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,9 +4,6 @@ from .problem import Problem, Test_case
 from .submission import Submission
 from .language import Programming_language
 
-# Цей список __all__ є опціональним, але вважається хорошою практикою.
-# Він явно визначає, які імена будуть експортовані, коли хтось
-# використовує "from app.models import *".
 __all__ = [
     "Base",
     "User",
